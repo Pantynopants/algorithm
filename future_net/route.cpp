4f62c34ca0146ae68c95bff8d69d07c0f79971eb
pantynopants <a1018791860@yeah.net>
2016-03-24 01:36:08 +0800

    v1.0

--- future_net/route.cpp
+++ future_net/route.cpp
@@ -1,21 +1,31 @@
 
 #include "route.h"
 #include "lib_record.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <iostream>
-using namespace std;
 
-const int maxnum = 100;
-const int maxint = 999999;
-const int inf = 999999;
 
-struct AtoB{ 
-    int dist;
-    int id;
-    int route[50];
-}; 
+
+
+
+
+//TODO 
+// IMPORTTANT!!!!
+// 0 function searchPath(...) return a wrong value , such as 0to19 ,
+// we can get it bug it print "no path"
+//
+// 1 存储方式由邻接矩阵转化为邻接表
+// 2 实现pdf中的算法
+// 3 注意输出路径结果为边的编号而不是顶点的编号
+// 
+//
+//
+
+
+
+
+
+
+
+
 /* n == end point
 *	v==start point
 */
@@ -74,25 +84,95 @@ int Dijkstra(int n, int v, int *dist, int *prev, AtoB c[maxnum][maxnum])
 */
 void searchPath(int *prev,int v, int u)
 {
+
     int que[maxnum];
     int tot = 1;
+
     que[tot] = u;
     tot++;
+
+
+    if (prev[u] == NULL)
+    {
+        printf("no path from %d to %d \n",v,u );
+        return;
+    }
+
+
     int tmp = prev[u];
     while(tmp != v)
     {
         que[tot] = tmp;
         tot++;
+
+        if (prev[tmp] == NULL)
+        {
+            printf("no path from %d to %d \n",v,u );
+            return;
+        }
+
         tmp = prev[tmp];
     }
     que[tot] = v;
-    for(int i=tot; i>=1; --i)
+    for(int i=tot; i>=1; --i){
         if(i != 1)
             cout << que[i] << " -> ";
         else
             cout << que[i] << endl;
+    }
 }
 
+
+
+
+int midPath(int IncludingSetLength, int IncludingSet[50], AtoB c[maxnum][maxnum])
+{
+
+    //cout << "get mid path" << endl;
+    int midDist = 0;
+    int distance[maxnum];     // 表示当前点到源点的最短路径长度
+    int prevpoint[maxnum];     // 记录当前点的前一个结点
+
+    for(int i=0; i<=200; ++i)
+        distance[i] = maxint;
+
+    for (int i = 0; i+1 < IncludingSetLength; ++i)
+    {
+            
+        /*
+        * TODO use if {}else{} to cut runtime 
+        *
+        *
+        */
+
+        // if (c[ IncludingSet[i] ][ IncludingSet[i+1] ].dist != inf)
+        // {
+        //     midDist += c[ IncludingSet[i] ][ IncludingSet[i+1] ].dist;
+        // }else{
+            midDist += Dijkstra(IncludingSet[i+1], IncludingSet[i], distance, prevpoint, c);
+            //cout << IncludingSet[i] << "到" << IncludingSet[i+1] <<"的路径: " << endl;
+            searchPath(prevpoint, IncludingSet[i], IncludingSet[i+1]);
+
+        
+        
+        
+    }
+    // for (int i = 0; i < IncludingSetLength; ++i)
+    // {
+            
+    //     printf( "access :%d  ", IncludingSet[i]);
+    // }
+    
+    if (midDist >= inf)
+    {
+        printf( "no result\n" );
+    }else{
+        printf( "distance :%d \n" ,midDist);
+    }
+    
+    
+    return midDist;
+}
 
 
 
@@ -100,7 +180,6 @@ void search_route(char *topo[5000], int edge_num, char *demand)
 {
     //unsigned short result[] = {2, 6, 4};
     
-
     AtoB distM[maxnum][maxnum];
     
     int begin;
@@ -115,7 +194,7 @@ void search_route(char *topo[5000], int edge_num, char *demand)
     *
 	*/
 
-    //TODO
+    //TODO use dist[] not matrix to change 600
 
 	for (int i = 0; i < 100; ++i)
 	{
@@ -221,63 +300,110 @@ void search_route(char *topo[5000], int edge_num, char *demand)
         token = strtok( NULL, ",|");
         i++;
     }
-    printf( "start :%d ", SourceID );
-    printf( "end :%d ", DestinationID );
+    printf( "start :%d \n", SourceID );
+    printf( "end :%d \n", DestinationID );
+    printf( "number :%d \n", IncludingSetLength);
+    // for (int i = 0; i < IncludingSetLength; ++i)
+    // {
+        
+    // 	printf( "access :%d ", IncludingSet[i] );
+
+    // }
+    
+
+    int arrNum = 1;
+    int midDist = inf;
+
+    int totalDist = inf;
+
+    int firstDist = inf;
+    int lastDist = inf;
+
+    int tempDist = 0;
+
+    int firstdistance[maxnum];     // 表示当前点到源点的最短路径长度
+    int firstprevpoint[maxnum];     // 记录当前点的前一个结点
+
+    for(int i=0; i<=edge_num/2; ++i)
+        firstdistance[i] = maxint;
+
+    int lastdistance[maxnum];     // 表示当前点到源点的最短路径长度
+    int lastprevpoint[maxnum];     // 记录当前点的前一个结点
+
+    for(int i=0; i<=edge_num/2; ++i)
+        lastdistance[i] = maxint;
+
+    int copyIncludingSet[50];
+
+    do 
+    {  
+        /* Dijkstra(int SourceID, int DestinationID, int distance[], int prevpoint[], AtoB distM[][]);*/
+
+        midDist = midPath(IncludingSetLength, IncludingSet, distM);
+        firstDist = Dijkstra(IncludingSet[0] , SourceID, firstdistance, firstprevpoint, distM);
+        lastDist = Dijkstra(DestinationID, IncludingSet[IncludingSetLength], lastdistance, lastprevpoint, distM);
+
+        tempDist = firstDist + midDist + lastDist;
+        cout << firstDist << " " << midDist << " " << lastDist << "\n" ;
+        if (tempDist < totalDist)
+        {
+            totalDist = tempDist;
+            //copy the shortest IncludingSet to the new array
+            //the last time we copied is the shortest IncludingSet
+            
+            //memcpy(copyIncludingSet,IncludingSet, IncludingSetLength*sizeof(int)); 
+
+            for (int i = 0; i < IncludingSetLength; ++i)
+            {
+                copyIncludingSet[i] = IncludingSet[i];
+            }
+            cout << "\n\n\n\n\n\n\n\n\n\n copy \n\n\n\n\n\n\n" ;
+            
+        }
+
+        arrNum++;
+            
+    }while (next_permutation(IncludingSet, (IncludingSet+IncludingSetLength)));
+
+    cout << "array number is :" << arrNum-1 << endl;
+    
+    printf("\n\n\n\n\n");
     for (int i = 0; i < IncludingSetLength; ++i)
     {
-        
-    	printf( "access :%d ", IncludingSet[i] );
-
+        printf("access %d", copyIncludingSet[i]);
     }
+    printf("\n\n\n\n\n");
 
-    /*
-    * algorithm start
-    *
-    */
+    midDist = midPath(IncludingSetLength, IncludingSet, distM);
 
- //    int nodeNum = 0;
+    firstDist = Dijkstra(IncludingSet[0] , SourceID, firstdistance, firstprevpoint, distM);
+    searchPath(firstprevpoint, SourceID, IncludingSet[0]);
 
- //    for (int i = 0; i < edge_num; ++i)
-	// {
-	// 	for (int j = 0; j < edge_num; ++j)
-	// 	{
-	// 		for (int k = 0; k < edge_num; ++k)
-	// 		{
-	// 			if (distM[j][k].dist > distM[j][i].dist + distM[i][k].dist)
-	// 			{
-	// 				distM[j][k].dist = distM[j][i].dist + distM[i][k].dist;
-	// 				distM[j][k].route[nodeNum] = i;
-					
-	// 				nodeNum++;				
-	// 			}
-	// 		}
-	// 	}
-	// }
+    lastDist = Dijkstra(DestinationID, IncludingSet[IncludingSetLength], lastdistance, lastprevpoint, distM);
+    searchPath(lastprevpoint, IncludingSet[IncludingSetLength], DestinationID);
 
-    int distance[maxnum];     // 表示当前点到源点的最短路径长度
-    int prevpoint[maxnum];     // 记录当前点的前一个结点
+    printf("\n\n\n\n\n");
 
-	for(int i=0; i<=edge_num/2; ++i)
-        distance[i] = maxint;
 
-    int case0;
 
-/* Dijkstra(int SourceID, int DestinationID, int distance[], int prevpoint[], AtoB distM[][]);*/
-    // you can set startpoint and endpoint as you want
-    case0 = Dijkstra(DestinationID, SourceID, distance, prevpoint, distM);
+
+ //    int distance[maxnum];     // 表示当前点到源点的最短路径长度
+ //    int prevpoint[maxnum];     // 记录当前点的前一个结点
+
+	// for(int i=0; i<=edge_num/2; ++i)
+ //        distance[i] = maxint;
+
+ //    int case0;
+
+ //    // you can set startpoint and endpoint as you want
+ //    case0 = Dijkstra(DestinationID, SourceID, distance, prevpoint, distM);
  
-    // 最短路径长度
-    cout << "startPoint到endPoint的最短路径长度: " << case0 << endl;
+ //    // 最短路径长度
+ //    cout << "startPoint到endPoint的最短路径长度: " << case0 << endl;
 
-	cout << "tartPoint到endPoint的的路径: " << endl;
-	searchPath(prevpoint, SourceID, DestinationID);
-//TODO 
-// 1 存储方式由邻接矩阵转化为邻接表
-// 2 实现pdf中的算法
-// 3 注意输出路径结果为边的编号而不是顶点的编号
-//
-//
-//
+	// cout << "tartPoint到endPoint的的路径: " << endl;
+	// searchPath(prevpoint, SourceID, DestinationID);
+
 /* wirte to file */
     for (int i = 0; i < 3; i++)
         record_result(*topo[i]);
--- future_net/route.h
+++ future_net/route.h
@@ -1,6 +1,34 @@
 #ifndef __ROUTE_H__
 #define __ROUTE_H__
 
+
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstdio>  
+#include <algorithm>  
+#include <cstring>  
+using namespace std;
+
+
+const int maxnum = 100;
+const int maxint = 999999;
+const int inf = 999999;
+
+
+
+struct AtoB{ 
+    int dist;
+    int id;
+    int route[50];
+}; 
+
+void searchPath(int *prev,int v, int u);
+
+int Dijkstra(int n, int v, int *dist, int *prev, AtoB c[maxnum][maxnum]);
+
+int midPath(int IncludingSetLength, int IncludingSet[50], AtoB c[maxnum][maxnum]);
+
 void search_route(char *graph[5000], int edge_num, char *condition);
 
 #endif
